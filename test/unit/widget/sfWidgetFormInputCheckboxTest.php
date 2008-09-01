e50fe3243ce87f7112215078f1acf7607b57d95e
nicolas <nicolas@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-09-01 10:27:05 +0000

    [1.1] added a way to set the "value" attribute value of a checkbox (added more tests to ensure strict BC)
    git-svn-id: http://svn.symfony-project.com/branches/1.1@11275 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/widget/sfWidgetFormInputCheckboxTest.php
+++ test/unit/widget/sfWidgetFormInputCheckboxTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(5, new lime_output_color());
+$t = new lime_test(7, new lime_output_color());
 
 $w = new sfWidgetFormInputCheckbox();
 
@@ -23,3 +23,6 @@ $t->is($w->render('foo', 0, array('value' => '0')), '<input type="checkbox" name
 
 $w = new sfWidgetFormInputCheckbox(array(), array('value' => 'bar'));
 $t->is($w->render('foo', null), '<input value="bar" type="checkbox" name="foo" id="foo" />', '->render() renders the widget as HTML');
+$t->is($w->render('foo', null, array('value' => 'baz')), '<input value="baz" type="checkbox" name="foo" id="foo" />', '->render() renders the widget as HTML');
+$t->is($w->render('foo', 'bar'), '<input value="bar" type="checkbox" name="foo" checked="checked" id="foo" />', '->render() renders the widget as HTML');
+
